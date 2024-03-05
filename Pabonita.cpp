e7a45197f451dd3226fe90ceeb1026c3b223e6e8
AIRA <144906782+Aira1805@users.noreply.github.com>
2024-03-06 00:03:59 +0800

    Add files via upload

---
+++ Pabonita.cpp
@@ -0,0 +1,27 @@
+// I. Rewrite the following if statements using the conditional operator: 
+
+// A.
+ticketPrice = (ages >= 16) ? 20 : 10;
+
+// B.
+cout << ((count % 10 == 0) ? count : count + " ") << endl;
+
+// II. Rewrite the following conditional expressions using if-else statements: 
+
+// A.
+if (x > 10)
+    score = 3 * scale;
+else
+    score = 4 * scale;
+
+// B.
+if (income > 10000)
+    tax = income * 0.2;
+else
+    tax = income * 0.17 + 1000;
+
+// C.
+if (number % 3 == 0)
+    cout << i << endl;
+else
+    cout << j << endl;
